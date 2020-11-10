ef9388221361204e72dea0ad73f1dcf67362923d
doublebirdsu <doublebirdsu@gmail.com>
2020-11-10 17:25:51 +0800

    Init Deep Learning.

---
+++ segmentation/yolov3_spp/models.py
@@ -0,0 +1,127 @@
+import torch
+import numpy as np
+
+from utils import torch_utils
+from utils.model import Model
+from utils.parse_config import parse_model_cfg
+
+ONNX_EXPORT = False
+
+
+class YOLOV3_SPP(Model):
+    # YOLOv3 object detection model
+
+    def __init__(self, cfg, img_size=(416, 416), verbose=False):
+        super(YOLOV3_SPP, self).__init__()
+        self.input_size = [img_size] * 2 if isinstance(img_size, int) else img_size
+        self.module_defs = parse_model_cfg(cfg)
+        self.module_list, self.routs = self.create_modules(self.module_defs, img_size, cfg)
+        self.yolo_layers = get_yolo_layers(self)
+
+        # YOLOV3_SPP Header https://github.com/AlexeyAB/darknet/issues/2914#issuecomment-496675346
+        self.version = np.array([0, 2, 5], dtype=np.int32)  # (int32) version info: major, minor, revision
+        self.seen = np.array([0], dtype=np.int64)  # (int64) number of images seen during training
+        self.info(verbose) if not ONNX_EXPORT else None  # print model description
+
+    def forward(self, x, augment=False, verbose=False):
+        if not augment:
+            return self.forward_once(x)
+        else:  # Augment images (inference and test only) https://github.com/ultralytics/yolov3/issues/931
+            img_size = x.shape[-2:]  # height, width
+            s = [0.83, 0.67]  # scales
+            y = []
+            for i, xi in enumerate((x,
+                                    torch_utils.scale_img(x.flip(3), s[0], same_shape=False),  # flip-lr and scale
+                                    torch_utils.scale_img(x, s[1], same_shape=False),  # scale
+                                    )):
+                y.append(self.forward_once(xi)[0])
+            y[1][..., :4] /= s[0]  # scale
+            y[1][..., 0] = img_size[1] - y[1][..., 0]  # flip lr
+            y[2][..., :4] /= s[1]  # scale
+
+            y = torch.cat(y, 1)
+            return y, None
+
+    def forward_once(self, x, augment=False, verbose=False):
+        img_size = x.shape[-2:]  # height, width
+        yolo_out, out = [], []
+        if verbose:
+            print('0', x.shape)
+            ver_str = ''
+
+        # Augment images (inference and test only)
+        if augment:  # https://github.com/ultralytics/yolov3/issues/931
+            nb = x.shape[0]  # batch size
+            s = [0.83, 0.67]
+            x = torch.cat((x,
+                           torch_utils.scale_img(x.flip(3), s[0]),  # flip-lr and scale
+                           torch_utils.scale_img(x, s[1]),  # scale
+                           ), 0)
+
+        for i, module in enumerate(self.module_list):
+            name = module.__class__.__name__
+            if name in ["WeightedFeatureFusion", "FeatureConcat"]:  # sum, concat
+                if verbose:
+                    l = [i - 1] + module.layers  # layers
+                    sh = [list(x.shape)] + [list(out[i].shape) for i in module.layers]  # shapes
+                    ver_str = ' >> ' + ' + '.join(['layer %g %s' % x for x in zip(l, sh)])
+                x = module(x, out)  # WeightedFeatureFusion(), FeatureConcat()
+            elif name == "YOLOLayer":
+                yolo_out.append(module(x, out))
+            else:  # run module directly, i.e. mtype = 'convolutional', 'upsample', 'maxpool', 'batchnorm2d' etc.
+                x = module(x)
+
+            out.append(x if self.routs[i] else [])
+            if verbose:
+                print('%g/%g %s -' % (i, len(self.module_list), name), list(x.shape), ver_str)
+                ver_str = ''
+
+        if self.training:  # train
+            return yolo_out
+        elif ONNX_EXPORT:  # export
+            # x = [torch.cat(x, 0) for x in zip(*yolo_out)]
+            # return x[0], torch.cat(x[1:3], 1)  # scores, boxes: 3780x80, 3780x4
+            p = torch.cat(yolo_out, dim=0)
+
+            # # 根据objectness虑除低概率目标
+            # mask = torch.nonzero(torch.gt(p[:, 4], 0.1), as_tuple=False).squeeze(1)
+            # # onnx不支持超过一维的索引（pytorch太灵活了）
+            # # p = p[mask]
+            # p = torch.index_select(p, dim=0, index=mask)
+            #
+            # # 虑除小面积目标，w > 2 and h > 2 pixel
+            # # ONNX暂不支持bitwise_and和all操作
+            # mask_s = torch.gt(p[:, 2], 2./self.input_size[0]) & torch.gt(p[:, 3], 2./self.input_size[1])
+            # mask_s = torch.nonzero(mask_s, as_tuple=False).squeeze(1)
+            # p = torch.index_select(p, dim=0, index=mask_s)  # width-height 虑除小目标
+            #
+            # if mask_s.numel() == 0:
+            #     return torch.empty([0, 85])
+
+            return p
+        else:  # inference or test
+            x, p = zip(*yolo_out)  # inference output, training output
+            x = torch.cat(x, 1)  # cat yolo outputs
+            if augment:  # de-augment results
+                x = torch.split(x, nb, dim=0)
+                x[1][..., :4] /= s[0]  # scale
+                x[1][..., 0] = img_size[1] - x[1][..., 0]  # flip lr
+                x[2][..., :4] /= s[1]  # scale
+                x = torch.cat(x, 1)
+
+            return x, p
+
+    def info(self, verbose=False):
+        """打印模型的信息
+            :param verbose:
+            :return:
+        """
+        torch_utils.model_info(self, verbose)
+
+
+def get_yolo_layers(model):
+    """获取网络中三个"YOLOLayer"模块对应的索引
+        :param model:
+        :return:
+    """
+    return [i for i, m in enumerate(model.module_list) if m.__class__.__name__ == 'YOLOLayer']  # [89, 101, 113]
---
+++ segmentation/yolov3_spp/utils/layers.py
@@ -0,0 +1,204 @@
+from typing import Any
+
+import torch
+import math
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+from utils.base_model import base_model
+
+
+def make_divisible(v, divisor):
+    """Function ensures all layers have a channel number that is divisible by 8
+        url https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+
+    Param:
+        v:
+        divisor:
+
+    Returns:
+
+    """
+    return math.ceil(v / divisor) * divisor
+
+
+class Flatten(base_model):
+    # Use after nn.AdaptiveAvgPool2d(1) to remove last 2 dimensions
+    @staticmethod
+    def forward(x):
+        return x.view(x.size(0), -1)
+
+
+class Concat(base_model):
+    # Concatenate a list of tensors along dimension
+    def __init__(self, dimension=1):
+        super(Concat, self).__init__()
+        self.d = dimension
+
+    def forward(self, x):
+        return torch.cat(x, self.d)
+
+
+class FeatureConcat(base_model):
+    def __init__(self, layers):
+        super(FeatureConcat, self).__init__()
+        self.layers = layers  # layer indices
+        self.multiple = len(layers) > 1  # multiple layers flag
+
+    def forward(self, x, outputs):
+        return torch.cat([outputs[i] for i in self.layers], 1) if self.multiple else outputs[self.layers[0]]
+
+
+class WeightedFeatureFusion(base_model):  # weighted sum of 2 or more layers https://arxiv.org/abs/1911.09070
+    """将多个特征矩阵的值进行融合
+    """
+
+    def __init__(self, layers, weight=False):
+        super(WeightedFeatureFusion, self).__init__()
+        self.layers = layers  # layer indices
+        self.weight = weight  # apply weights boolean
+        self.n = len(layers) + 1  # number of layers 融合的特征矩阵个数
+        if weight:
+            self.w = nn.Parameter(torch.zeros(self.n), requires_grad=True)  # layer weights
+
+    def forward(self, x, outputs):
+        # Weights
+        if self.weight:
+            w = torch.sigmoid(self.w) * (2 / self.n)  # sigmoid weights (0-1)
+            x = x * w[0]
+
+        # Fusion
+        nx = x.shape[1]  # input channels
+        for i in range(self.n - 1):
+            a = outputs[self.layers[i]] * w[i + 1] if self.weight else outputs[self.layers[i]]  # feature to add
+            na = a.shape[1]  # feature channels
+
+            # Adjust channels
+            # 根据相加的两个特征矩阵的channel选择相加方式
+            if nx == na:  # same shape 如果channel相同，直接相加
+                x = x + a
+            elif nx > na:  # slice input 如果channel不同，将channel多的特征矩阵砍掉部分channel保证相加的channel一致
+                x[:, :na] = x[:, :na] + a  # or a = nn.ZeroPad2d((0, 0, 0, 0, 0, dc))(a); x = x + a
+            else:  # slice feature
+                x = x + a[:, :nx]
+
+        return x
+
+
+class MaxPool2D(nn.MaxPool2d):
+    def __init__(self, kernel_size, stride, padding='valid', padding_value=0.):
+        """MaxPool2D
+            padding
+
+        :type padding: str
+        :type padding_value: float
+
+        :param kernel_size: 内核尺寸
+        :param stride: 步长
+        :param padding: 'valid' or 'same', 'valid': 不进行补充, 'same': 补足. default 'valid'
+        :param padding_value: float
+        """
+        super(MaxPool2D, self).__init__(
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=(kernel_size - 1) // 2 if padding == 'same' and padding_value == 0. else 0)
+        self.pad = None
+        if padding == 'same':  # Padding
+            if kernel_size == 2 and stride == 1:
+                self.pad = nn.ZeroPad2d((0, 1, 0, 1))  # l, r, t, b; yoloV3-tiny
+            elif padding_value != 0.:
+                self.pad = nn.ConstantPad2d((kernel_size - 1) // 2, padding_value)
+
+    def forward(self, x):
+        return super().forward(self.pad(x) if self.pad else x)
+
+    def _forward_unimplemented(self, *input: Any) -> None:
+        return super(MaxPool2D, self)._forward_unimplemented(*input)
+
+
+class MixConv2d(base_model):  # MixConv: Mixed Depthwise Convolutional Kernels https://arxiv.org/abs/1907.09595
+    def __init__(self, in_ch, out_ch, kernel_size=(3, 5, 7), stride=1, dilation=1, bias=True, method='equal_params'):
+        super(MixConv2d, self).__init__()
+
+        groups = len(kernel_size)
+        if method == 'equal_ch':  # equal channels per group
+            i = torch.linspace(0, groups - 1E-6, out_ch).floor()  # out_ch indices
+            ch = [(i == g).sum() for g in range(groups)]
+        else:  # 'equal_params': equal parameter count per group
+            b = [out_ch] + [0] * groups
+            a = np.eye(groups + 1, groups, k=-1)
+            a -= np.roll(a, 1, axis=1)
+            a *= np.array(kernel_size) ** 2
+            a[0] = 1
+            ch = np.linalg.lstsq(a, b, rcond=None)[0].round().astype(int)  # solve for equal weight indices, ax = b
+
+        self.m = nn.ModuleList([
+            nn.Conv2d(in_channels=in_ch,
+                      out_channels=ch[g],
+                      kernel_size=kernel_size[g],
+                      stride=stride,
+                      padding=kernel_size[g] // 2,  # 'same' pad
+                      dilation=dilation,
+                      bias=bias) for g in range(groups)])
+
+    def forward(self, x):
+        return torch.cat([m(x) for m in self.m], 1)
+
+
+# Activation functions below -------------------------------------------------------------------------------------------
+class SwishImplementation(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x):
+        ctx.save_for_backward(x)
+        return x * torch.sigmoid(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        x = ctx.saved_tensors[0]
+        sx = torch.sigmoid(x)  # sigmoid(ctx)
+        return grad_output * (sx * (1 + x * (1 - sx)))
+
+
+class MishImplementation(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, **kwargs):
+        ctx.save_for_backward(x, **kwargs)
+        return x.mul(torch.tanh(F.softplus(x)))  # x * tanh(ln(1 + exp(x)))
+
+    @staticmethod
+    def backward(ctx, grad_output, **kwargs):
+        x = ctx.saved_tensors[0]
+        sx = torch.sigmoid(x)
+        fx = F.softplus(x).tanh()
+        return grad_output * (fx + x * sx * (1 - fx * fx))
+
+
+class MemoryEfficientSwish(base_model):
+    @staticmethod
+    def forward(x):
+        return SwishImplementation.apply(x)
+
+
+class MemoryEfficientMish(base_model):
+    @staticmethod
+    def forward(x):
+        return MishImplementation.apply(x)
+
+
+class Swish(base_model):
+    @staticmethod
+    def forward(x):
+        return x * torch.sigmoid(x)
+
+
+class HardSwish(base_model):  # https://arxiv.org/pdf/1905.02244.pdf
+    @staticmethod
+    def forward(x):
+        return x * F.hardtanh(x + 3, 0., 6., True) / 6.
+
+
+class Mish(base_model):  # https://github.com/digantamisra98/Mish
+    @staticmethod
+    def forward(x):
+        return x * F.softplus(x).tanh()

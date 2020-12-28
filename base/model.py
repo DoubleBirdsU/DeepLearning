74e902abb8a891c8b84dce8698e922ecd12e9fd8
doublebirdsu <doublebirdsu@gmail.com>
2020-12-28 16:27:18 +0800

    增加新的 callbacks 类型 DatasetCheck
    DatasetCheck: 处理 loss 输入前 y_pred, y_true 数据

--- base/model.py
+++ base/model.py
@@ -49,6 +49,7 @@ class ModelCheckpoint(object):
         # ckpt
         self.ckpt = self.check_ckpt_complete(self.ckpt_read(self.root_path, self.ckpt_filename))
         self.accuracy_best = self.ckpt['accuracy'] if 'accuracy' in self.ckpt else 0
+        self.loss_best = self.ckpt['loss'] if 'loss' in self.ckpt else float('inf')
         self.ckpt['filename'] = os.path.abspath(self.filepath)
         self.ckpt['save_best_only'] = self.save_best_only
 
@@ -60,6 +61,7 @@ class ModelCheckpoint(object):
             net (NNet):
         """
         acc_cur = kwargs.pop('accuracy') if 'accuracy' in kwargs else 0.  # 取出 accuracy 参数
+        loss = kwargs.pop('loss') if 'loss' in kwargs else float('inf')
         if self.save_weights_only:  # parameters
             f = net.state_dict()
             self.ckpt['save_mode'] = self.Weight
@@ -77,26 +79,29 @@ class ModelCheckpoint(object):
         # Save last 存在问题: 如果新训练的覆盖了旧的概率模型
         suffix_last = self.get_suffix(index=index_last, mode=mode)
         file_last = self.filepath + suffix_last
-        acc_change = (self.accuracy_best < acc_cur or (self.save_best_only and not os.path.exists(file_last)))
+        param_change = (self.accuracy_best < acc_cur or loss < self.loss_best or
+                        (self.save_best_only and not os.path.exists(file_last)))
         torch.save(f, self.filepath + suffix_last, self.pickle_module, self.pickle_protocol,
                    self._use_new_zipfile_serialization)
 
-        # Accuracy
+        # Accuracy and Loss
         suffix_best = self.ckpt['suffix_best'] if 'suffix_best' in self.ckpt else suffix_last
-        if acc_change:
+        if param_change:
             suffix_best = suffix_last
             self.accuracy_best = acc_cur
+            self.loss_best = float(loss.to(torch.device('cpu')).numpy()) if isinstance(loss, torch.Tensor) else loss
 
         # Save best
         if self.save_best_only:
             suffix_best = self.get_suffix(index=index_best)
             file_best = self.filepath + suffix_best
-            if acc_change or not os.path.exists(file_best):
+            if param_change or not os.path.exists(file_best):
                 torch.save(f, file_best, self.pickle_module, self.pickle_protocol,
                            self._use_new_zipfile_serialization)
 
         # Dump ckpt
         self.ckpt['accuracy'] = self.accuracy_best
+        self.ckpt['loss'] = self.loss_best
         self.ckpt['suffix_best'] = suffix_best
         self.ckpt['suffix_last'] = suffix_last
         self.ckpt_dump(self.ckpt, os.path.join(self.root_path, self.ckpt_filename))
@@ -131,12 +136,26 @@ class ModelCheckpoint(object):
 
     @staticmethod
     def check_ckpt_complete(ckpt):
-        if 'filename' in ckpt and 'suffix_best' in ckpt and 'suffix_last' in ckpt:
+        if ckpt is not None and 'filename' in ckpt and 'suffix_best' in ckpt and 'suffix_last' in ckpt:
             file_best = ckpt['filename'] + ckpt['suffix_best']
             file_last = ckpt['filename'] + ckpt['suffix_last']
             if os.path.exists(file_best) and os.path.exists(file_last):
                 return ckpt
         return dict()
+
+
+class DatasetCheck(nn.Module):
+    """DatasetCheck
+
+        处理 loss 输入前 y_pred, y_true 数据
+
+    """
+    def __init__(self, trans_fn=None):
+        super(DatasetCheck, self).__init__()
+        self.trans_fn = trans_fn
+
+    def __call__(self, y_pred, y_true):
+        return self.trans_fn(y_pred, y_true) if self.trans_fn is not None else (y_pred, y_true)
 
 
 class NNet(Module):
@@ -147,6 +166,7 @@ class NNet(Module):
         self.device = None
         self.metrics = None
         self.checkpoint_fn = None
+        self.checkdata_fn = None
         self.epoch = 0
         self.epochs = 0
         self._callbacks = None
@@ -228,11 +248,11 @@ class NNet(Module):
             self.epoch = epoch
             self.train_once(train_data, batch_size)
             if epoch % validation_freq == 0 or epoch == epochs:
-                _, acc_cur = self.valid_once(validation_data)
+                loss, acc_cur = self.valid_once(validation_data)
 
                 # checkpoint
                 if self.checkpoint_fn is not None:
-                    self.checkpoint_fn(self, accuracy=acc_cur)
+                    self.checkpoint_fn(self, accuracy=acc_cur, loss=loss)
 
     def train_once(self, train_loader, batch_size):
         """train_once
@@ -274,7 +294,7 @@ class NNet(Module):
             for data, y_true in valid_loader:
                 data, y_true = data.to(self.device), y_true.to(self.device)
                 y_pred = self(data)
-                loss_mean += self.loss(y_pred, y_true, reduction='sum').item()  # sum up batch loss
+                loss_mean += self.loss(y_pred, y_true)
                 correct += self._get_correct(y_pred, y_true) if self.metrics else 0.
 
         # 输出信息
@@ -380,6 +400,8 @@ class NNet(Module):
         for fn in callbacks:
             if isinstance(fn, ModelCheckpoint):
                 self.checkpoint_fn = fn
+            elif isinstance(fn, DatasetCheck):
+                self.checkdata_fn = fn
 
     def _print_cover(self, loss, correct, count_data, cur_batch, count_batch):
         schedule = self._get_schedule(1. * cur_batch / count_batch)
